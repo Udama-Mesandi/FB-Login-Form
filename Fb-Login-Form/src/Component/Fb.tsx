1c608ea246c405ceecd430b290dc3f59cfac0982
UDAMA_MESANDI <wumpeiris@gmail.com>
2024-02-18 12:05:26 +0530

    create Login Form

--- Fb-Login-Form/src/Component/Fb.tsx
+++ Fb-Login-Form/src/Component/Fb.tsx
@@ -12,7 +12,32 @@ function Fb() {
 
 
 
-                
+                <div className='fb-form  col-lg-6 col-md-6 col-sm-12'>
+                    <form>
+                        <fieldset>
+                        <input type='text'  id="email-phone" className="form-control" placeholder='Email or Phone Number'></input><br></br>
+                        <input type='text'  id="email-phone" className="form-control" placeholder='Password'></input><br></br>
+                        <div className="d-grid gap-20 col-12 mx-auto">
+                        <button className='btn btn-primary btn-lg'>Login</button><br></br>
+                        
+                            
+    
+                        </div>
+                        <a href='#'>Forget Password</a><hr></hr>
+                        
+                        <div className="d-grid gap-20 col-6 mx-auto">
+                        <button  id='create-account' className='btn btn-   btn-lg'>Create Account</button><br></br>
+                        
+                            
+    
+                        </div>
+                        </fieldset>
+                        
+
+
+                    </form>
+
+                </div>
             </div>
 
 
