b48a7fe4d35f38799cddd5be532168f563bc40d3
UDAMA_MESANDI <wumpeiris@gmail.com>
2024-02-18 12:04:05 +0530

    create Facebook Title

---
+++ Fb-Login-Form/src/Component/Fb.tsx
@@ -0,0 +1,26 @@
+import './Fb.css'
+
+function Fb() {
+    return (
+        <div className='container'>
+
+            <div className='row'>
+                <div className='fb_desc col-lg-6  col-md-6 col-sm-12 p-5 '>
+                    <h1 id='titel'>FaceBook</h1>
+                    <p id ='desc'>Facebook helps you connect and share with the people in your life.</p>
+                </div>
+
+
+
+                
+            </div>
+
+
+
+
+
+        </div>
+    );
+}
+
+export default Fb
